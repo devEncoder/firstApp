eee91102ad9815c8bb99630eb5bb2e6c44e60c9f
devEncoder <codeur44@gmail.com>
2026-01-05 20:14:39 +0100

    Update calculation html page UX

--- implementation/script/calculation.js
+++ implementation/script/calculation.js
@@ -1,11 +1,17 @@
 /*** Get Form value */
-function getCalculationValue(){
-    console.log('getSimulationValue');
-    let C = document.getElementById("borrowedCapital").value;
-    let annualRate = (document.getElementById("annualRate").value)/100;
-    let n = document.getElementById("MonthDuration").value;
-    let t = annualRate/12;
-    var MonthlyRepayment =  C*(t/(1-((1+t)**(-n))));
+function getCalculationValue() {
+  console.log("getSimulationValue");
+  let C = document.getElementById("borrowedCapital").value;
+  let annualRate =
+    document.getElementById("annualRate").value.replaceAll(",", ".") / 100;
+  let n = document.getElementById("MonthDuration").value;
+  let t = annualRate / 12;
+  var MonthlyRepayment = C * (t / (1 - (1 + t) ** -n));
 
-    document.getElementById("output").innerHTML = MonthlyRepayment.toFixed(2);
+  if (isNaN(MonthlyRepayment)) {
+    document.getElementById("output").innerHTML = "Bad Entry"
+  } else {
+    document.getElementById("output").innerHTML =
+      MonthlyRepayment.toFixed(1) + "€ for " + n + " years";
+  }
 }
