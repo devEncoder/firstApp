991150d3d4d5a1b256551375824e891a38ef66c5
devEncoder <codeur44@gmail.com>
2026-01-08 08:32:38 +0100

    update calculation page

--- implementation/script/calculation.js
+++ implementation/script/calculation.js
@@ -1,17 +1,26 @@
 /*** Get Form value */
 function getCalculationValue() {
   console.log("getSimulationValue");
-  let C = document.getElementById("borrowedCapital").value;
+  let C = document.getElementById("borrowedCapital").value.replaceAll(",", ".");
   let annualRate =
     document.getElementById("annualRate").value.replaceAll(",", ".") / 100;
-  let n = document.getElementById("MonthDuration").value;
+  let n = document.getElementById("MonthDuration").value.replaceAll(",", ".");
   let t = annualRate / 12;
   var MonthlyRepayment = C * (t / (1 - (1 + t) ** -n));
 
+  var annualDuration = Math.floor(n / 12);
+  var moduloMonthly = n % 12;
+  console.log(n);
+  console.log(annualDuration);
+  console.log(moduloMonthly);
   if (isNaN(MonthlyRepayment)) {
-    document.getElementById("output").innerHTML = "Bad Entry"
+    document.getElementById("output").innerHTML = "Bad Entry";
+     document.getElementById("output2").innerHTML = "";
   } else {
     document.getElementById("output").innerHTML =
-      MonthlyRepayment.toFixed(1) + "€ for " + n + " years";
+      //MonthlyRepayment.toFixed(1) + "€ for " + n + " months";
+      MonthlyRepayment.toFixed(1) + " € ";
+    document.getElementById("output2").innerHTML =
+       annualDuration + " years " + moduloMonthly + " months ";
   }
 }
